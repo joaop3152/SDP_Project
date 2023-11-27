35609798a7e96ddb8986b5d8f0c70751272e7a47
Du4rteRodrigues <duarte.m.rodrigues@sapo.pt>
2023-11-27 12:50:33 +0000

    innital commit

---
+++ Application/client.py
@@ -0,0 +1,17 @@
+# client.py
+from network import create_socket
+
+def connect_to_server(host, port):
+    client_socket = create_socket()
+    client_socket.connect((host, port))
+    return client_socket
+
+def send_name(client_socket, name):
+    client_socket.sendall(name.encode())
+
+def send_message(client_socket, target, message):
+    client_socket.sendall(f"{target}:{message}".encode())
+
+def receive_message(client_socket):
+    data = client_socket.recv(1024)
+    return data.decode()
---
+++ Presentation/run_server.py
@@ -0,0 +1,7 @@
+# run_server.py
+from Application.server import start_server
+
+HOST = '0.0.0.0'
+PORT = 12345
+
+start_server(HOST, PORT)
