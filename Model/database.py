9eecf8ada3f91764e9238a78848a925cc2147aca
Gonçalo Vidal <30009146@students.ual.pt>
2024-01-25 12:39:58 +0000

    insert note

--- Model/database.py
+++ Model/database.py
@@ -15,14 +15,14 @@ def create_db_connection():
         print(f"Error connecting to the database {err}")
         return None
 
-# def insert_message(sender, receiver, message): change to our app 
-#     db_connection = create_db_connection()
-#     if db_connection is not None:
-#         cursor = db_connection.cursor()
-#         insert_query = """
-#         INSERT INTO messages (sender, receiver, message) VALUES (%s, %s, %s)
-#         """
-#         cursor.execute(insert_query, (sender, receiver, message))
-#         db_connection.commit()
-#         cursor.close()
-#         db_connection.close()
+def insert_note(title, body, user_id):
+    db_connection = create_db_connection()
+    if db_connection is not None:
+        cursor = db_connection.cursor()
+        insert_query = """
+        INSERT INTO note (title, body, user_id) VALUES (%s, %s, %d)
+        """
+        cursor.execute(insert_query, (title, body, user_id))
+        db_connection.commit()
+        cursor.close()
+        db_connection.close()
