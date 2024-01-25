1663cfd9fc09ef36bffdeae1b48cd6b2dd3dd289
Gonçalo Vidal <30009146@students.ual.pt>
2024-01-25 13:11:58 +0000

    insert user

--- Model/database.py
+++ Model/database.py
@@ -15,14 +15,34 @@ def create_db_connection():
         print(f"Error connecting to the database {err}")
         return None
 
+# Database - Note
 def insert_note(title, body, user_id):
     db_connection = create_db_connection()
     if db_connection is not None:
         cursor = db_connection.cursor()
-        insert_query = """
-        INSERT INTO note (title, body, user_id) VALUES (%s, %s, %d)
-        """
+        insert_query = f"INSERT INTO note (title, body, user_id) VALUES ({title}, {body}, {user_id})"
+
         cursor.execute(insert_query, (title, body, user_id))
         db_connection.commit()
         cursor.close()
         db_connection.close()
+
+#################################
+#################################
+
+# Database - User
+def insert_user(username, password):
+    db_connection = create_db_connection()
+    if db_connection is not None:
+        cursor = db_connection.cursor()
+        insert_query = """
+        INSERT INTO user (username, password) VALUES (%s, %s)
+        """
+        cursor.execute(insert_query, (username, password))
+        db_connection.commit()
+        cursor.close()
+        db_connection.close()
+
+
+#################################
+#################################
