081e73e7f0aad2db3cbd85fb49edca3f2d6089d5
Weronika18991 <s18991@pjwstk.edu.pl>
2024-01-26 23:30:54 +0000

    separate database connection to method to reuse it

--- Model/database.py
+++ Model/database.py
@@ -34,14 +34,6 @@ def insert_note(title, body, user_id):
 
 # Database - User
 def insert_user(username, password):
-    db_connection = create_db_connection()
-    if db_connection is not None:
-        cursor = db_connection.cursor()
-        insert_query = """
-        INSERT INTO user (username, password) VALUES (%s, %s)
-        """
-        cursor.execute(insert_query, (username, password))
-        db_connection.commit()
-        cursor.close()
-        db_connection.close()
+    query = "INSERT INTO user (username, password) VALUES (%s, %s)"
+    perform_query(query, (username, password))
 
